b8536edf92fceff548296098bf8f98b0ee531f76
samimuhajir666-cmd <samimuhajir666@gmail.com>
2026-05-08 17:43:45 +0500

    Add snow effect on successful email notification

--- data.py
+++ data.py
@@ -110,6 +110,7 @@ with st.form("admission_form_main", clear_on_submit=True):
             
             if email_status:
                 st.balloons()
+                st.snow()     
                 st.success(f"Mubarak ho {name}! Aapka data save ho gaya aur Admin ko email bhej di gayi hai.")
             else:
                 st.warning(f"Data save ho gaya hai, lekin email bhejney mein masla hua.")
